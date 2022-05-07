711a4351046d18e69ceb0f0f6fb8e896b0591237
gustavocontin1 <contin.gustavo1@gmail.com>
2022-05-07 19:16:30 -0300

    agregado la validacion de inputs

--- javascript/promedio.js
+++ javascript/promedio.js
@@ -6,12 +6,31 @@ let resultadoNota
 let baseDatos = []
 let dato 
 const divPromedio = document.getElementById("promedio")
-let num4 = document.getElementById('num1')
+let inputs = document.querySelectorAll('#miFormulario input')
+const expresion = {
+    numeros: /^[1-9][1-9]{1,9}$/
+}
+//Validar inputs numeros 
+const validarFormulario = (e) => {
+            if(expresion.numeros.test(e.target.value)){
+                document.getElementById('grupo__numero').classList.add('formulario__grupo-incorrecto')
+                document.querySelector('#grupo__numero .formulario__input-error').classList.add('formulario__input-error-activo')
+            } else { 
+                document.getElementById('grupo__numero').classList.remove('formulario__grupo-incorrecto')
+                document.querySelector('#grupo__numero .formulario__input-error').classList.remove('formulario__input-error-activo')
+            }
+}
+
+inputs.forEach((input) =>{
+    input.addEventListener('keyup', validarFormulario)
+    input.addEventListener('blur', validarFormulario)
+})
 
 
 //Funcion para sacar el promedio
 btnTotal.onclick = function realizarPromedio(){
     const aprobado = 7
+    const mayor = 11
     nombreAlumno = document.getElementById('nom').value
     let nota1 = parseInt(document.getElementById('num1').value)
     let nota2 = parseInt(document.getElementById('num2').value)
@@ -26,9 +45,8 @@ btnTotal.onclick = function realizarPromedio(){
             icon: "success",
         });
         baseDatos.push("Nombre: " + nombreAlumno + ", Promedio: " + resultadoNota )
-    }else {
+    }else{
         console.log("Nota final de " + nombreAlumno + " es " + resultadoNota + " esta desaprobado" )
-        //alert (nombreAlumno + " tu nota es " + resultadoNota + ", estas desaprobado")
         swal({
             title: "Sigue Intentando",
             text: nombreAlumno + " tu nota es " + resultadoNota + ", estas desaprobado!",
@@ -37,14 +55,21 @@ btnTotal.onclick = function realizarPromedio(){
         baseDatos.push("Nombre: " + nombreAlumno + ", Promedio: " + resultadoNota )
         }
 
-for(const promedio of baseDatos){
+    const texto = "Nombre: " + nombreAlumno + ", Promedio: " + resultadoNota
+    baseDatos.push(texto)
     const resultado = document.createElement("li")
-    resultado.innerHTML = promedio
+    resultado.innerHTML = texto
     divPromedio.appendChild(resultado)
 
-    let total = []
-    total.push(baseDatos)
-    localStorage.setItem("Promedios", JSON.stringify(total)) }
+    if(resultadoNota >= mayor){
+        swal({
+            title: "Ingrese nuevamente una nota",
+            text: "La nota tiene que ser numeros entre el 1 al 10, intentalo nuevamente!",
+            icon: "warning",
+        })
+    }
+    
+    localStorage.setItem("Promedios", JSON.stringify(baseDatos)) 
 }
 
 
@@ -56,7 +81,10 @@ document.getElementById("btnTotal").addEventListener("click", function(e){
 miFormulario.btnBorrar.addEventListener('click', function (e) { 
     if (!confirm("Deseas limpiar lo escrito?")
     ) e.preventDefault()
-    else console.log('Borrado')
+    else {
+        document.getElementById('grupo__numero').classList.remove('formulario__grupo-incorrecto')
+        document.querySelector('#grupo__numero .formulario__input-error').classList.remove('formulario__input-error-activo')
+    }
 })
 
 
